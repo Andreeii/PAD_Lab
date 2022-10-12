23ae21c34bfbdabb3af15ee14ce4acf98fa98a51
Turcanu <andriesturcanu@gmail.com>
2022-10-12 21:30:04 +0300

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RestSharp;
+using WebApp1.Models;
+
+namespace WebApp1.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HomeController : ControllerBase
+    {
+
+        [HttpGet("getAllMovies")]
+        public IActionResult Get()
+        {
+            var client = new RestClient();
+            var request = new RestRequest("https://localhost:7257/WeatherForecast", Method.Get);
+            var response = client.Execute(request);
+            return Ok(response.Content);
+        }
+
+        [HttpPost("postMovie")]
+        public IActionResult Post(Movie movie)
+        {
+            var client = new RestClient();
+            var request = new RestRequest("https://localhost:7257/WeatherForecast/postMovie", Method.Post);
+            request.AddJsonBody(movie);
+            var response = client.Execute(request);
+            return Ok(response.StatusCode);
+        }
+    }
+}
---
+++ Models/Movie.cs
@@ -0,0 +1,10 @@
+﻿namespace WebApp1.Models
+{
+    public class Movie
+    {
+        public string Title { get; set; }
+        public long Year { get; set; }
+        public string DirectorName { get; set; }
+        public int StartNumber { get; set; }
+    }
+}
